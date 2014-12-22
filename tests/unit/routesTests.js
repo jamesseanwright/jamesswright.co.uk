25e367843f77d7f88049962c230dcd968cb0b433
James Wright <james.wright@bskyb.com>
2014-12-22 18:13:01 +0000

    Wrote more routes tests

--- tests/unit/routesTests.js
+++ tests/unit/routesTests.js
@@ -1,11 +1,14 @@
 var should = require('should'),
 	sinon = require('sinon'),
 	fs = require('fs'),
-	mockViews = ['view1.html'],
+	mockViews = ['index.html', 'view1.html'],
 	mockReq = { params: {} },
 	mockRes = { render: function (view) {} },
 	mockRender,
+	mockMiddleware = { next: function (data) {} },
 	getView;
+
+require('../../utils/polyfills')();
 
 describe('the site\'s routes', function () {
 	describe('The getView route', function () {
@@ -22,9 +25,27 @@ describe('the site\'s routes', function () {
 		it('should render a valid view', function () {
 			mockReq.params.viewName = 'view1';
 			mockRender = sinon.mock(mockRes).expects('render').once().withArgs(mockReq.params.viewName + '.html');
-			getView(mockReq, mockRes, function() {});
+			getView(mockReq, mockRes, function () {});
 
 			mockRender.verify();
+			mockRes.render.restore();
+		});
+
+		it('should invoke next for a nonexistent view', function () {
+			mockReq.params.viewName = 'view2';
+			mockNext = sinon.mock(mockMiddleware).expects('next').once().withArgs(new Error(404));
+			getView(mockReq, {}, mockMiddleware.next);
+
+			mockNext.verify();
+		});
+
+		it('should render index.html when viewName is falsy', function () {
+			mockReq.params.viewName = '';
+			mockRender = sinon.mock(mockRes).expects('render').once().withArgs('index.html');
+			getView(mockReq, mockRes, function () {});
+
+			mockRender.verify();
+			mockRes.render.restore();
 		});
 	});
 });
