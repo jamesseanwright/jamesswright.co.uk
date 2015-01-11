67fb8519033f0cea4fedf831e453c3c61e63fad7
James Wright <james@jamesswright.co.uk>
2015-01-11 12:00:18 +0000

    handleError middleware tests now in respective suite

---
+++ tests/unit/routes/handleErrorTests.js
@@ -0,0 +1,15 @@
+var handleError = require('../../../routes/handleError');
+var fakeRes = { status: function (code) {}, render: function (view, data) {} };
+var mockRender;
+
+describe('the handleError middleware', function () {
+	it('should render a error view with the correct HTTP status for the given code', function () {
+		sinon.stub(fakeRes, 'status').withArgs('404').returns(fakeRes);
+		mockRender = sinon.mock(fakeRes).expects('render').once().withArgs('error/404.html');
+
+		handleError(new Error(404), {}, fakeRes, function () {});
+
+		mockRender.verify();
+		fakeRes.status.restore();
+	});
+});
--- tests/unit/routesTests.js
+++ tests/unit/routesTests.js
@@ -15,40 +15,6 @@ var fs = require('fs'),
 require('../../utils/polyfills')();
 
 describe('the site\'s routes', function () {
-	afterEach(function () {
-		delete mockReq.params.viewName;
-
-		if (fs.readdirSync.restore) {
-			fs.readdirSync.restore();
-		}
-
-		if (mockRes.render.restore) {
-			mockRes.render.restore();
-		}
-		
-		if (mockRes.status.restore) {
-			mockRes.status.restore();
-		}
-	});		
-
-	
-
-	describe('the handleError middleware', function () {
-		beforeEach(function () {
-			handleError = require('../../routes/handleError');
-		});
-
-		it('should render a error view with the correct HTTP status for the given code', function () {
-			mockStatus = sinon.mock(mockRes).expects('status').once().withArgs('404').returns(mockRes);
-			mockRender = sinon.mock(mockRes).expects('render').once().withArgs('error/404.html');
-
-			handleError(new Error(404), {}, mockRes, function () {});
-
-			mockStatus.verify();
-			mockRender.verify();
-		});
-	});
-
 	describe('the getProjects route', function () {
 		beforeEach(function () {
 			getProjects = require('../../routes/getProjects');
