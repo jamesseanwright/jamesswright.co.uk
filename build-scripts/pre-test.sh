50db3e82725e82744d713a2c8bd2d32fbd53dcc0
James Wright <james@jamesswright.co.uk>
2015-03-31 19:44:45 +0100

    attempt to view pwd

--- build-scripts/pre-test.sh
+++ build-scripts/pre-test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm install -g grunt-cli && npm install -g bower && bower i && grunt uglify
+npm install -g grunt-cli && npm install -g bower && bower i && echo "Current dir:" && ls && grunt uglify
