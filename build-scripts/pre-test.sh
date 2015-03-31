5dace3e0cb38971cf816c6796b8fb333f48fdc14
James Wright <james@jamesswright.co.uk>
2015-03-31 19:57:52 +0100

    adding npm i to pre-test script

--- build-scripts/pre-test.sh
+++ build-scripts/pre-test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm install -g grunt-cli && npm install -g bower && bower i && echo "Current dir:" && ls && grunt uglify
+npm install -g grunt-cli && npm install -g bower && npm i && bower i && grunt uglify
