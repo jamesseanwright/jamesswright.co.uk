af6719507bd4eefcb28390cd7150d1dd715fd61e
James Wright <james@jamesswright.co.uk>
2015-03-31 18:52:26 +0100

    Added rudimentary dist script

---
+++ build-dist.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+echo "Building dist files..."
+mkdir -p dist
+rm -rf dist/*
+cp server.js package.json bower.json dist/
+
+cp -R data/ dist/
+cp -R init/	dist/
+cp -R models/ dist/
+cp -R public/ dist/
+rm -R dist/public/vendor/ 
+cp -R routes/ dist/
+cp -R sass/ dist/
+cp -R utils/ dist/
+cp -R views/ dist/
