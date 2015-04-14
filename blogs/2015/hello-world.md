b886f3475bd1dc72aa0c763919a262b6eb99d4e4
James Wright <james@jamesswright.co.uk>
2015-04-14 21:15:52 +0100

    @jw basic styling and deep linking of posts

--- blogs/2015/hello-world.md
+++ blogs/2015/hello-world.md
@@ -1,3 +1,5 @@
-## Hello World
+### [Hello World](/blog/hello-world)
+
+<time datetime="2015-04-14">14th April 2015</time>
 
 This is a test blog entry written in Markdown!
--- routes/getBlog.js
+++ routes/getBlog.js
@@ -5,34 +5,43 @@ var fs = require('fs');
 var Showdown = require('showdown');
 var converter = new Showdown.converter();
 
-function convert(file, posts) {
-	fs.readFile(file, function (err, content) {
-		if (err) {
-			next(err);
-			return;
-		}
+function convert(file) {
+	return new Promise(function (resolve, reject) {
+		fs.readFile(file, function (err, content) {
+			if (err) {
+				reject(err);
+			}
 
-		posts.push(converter.makeHtml(content.toString()));
+			resolve(converter.makeHtml(content.toString()));
+		});
 	});
+	
 }
 
 module.exports = function (req, res, next) {
 	var posts = [];
 
-	glob('blogs/**/*.md', function (err, files) {
+	glob('blogs/**/' + (req.params.slug || '*') + '.md', function (err, files) {
+		if (!files.length) {
+			next(new Error(404));
+			return;
+		}
+
 		if (err) {
 			next(err);
 			return;
 		}
 
 		files.forEach(function (file, i) {
-			convert(file, posts);
+			convert(file).then(function (markdown) {
+				posts.push(markdown);
 
-			if (i === files.length - 1) {
-				res.render('blog.html', {
-					posts: posts
-				});
-			}
+				if (i === files.length - 1) {
+					res.render('blog.html', {
+						posts: posts
+					});
+				}
+			}).catch(next);
 		});
 	});
 };
