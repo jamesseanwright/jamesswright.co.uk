5cd97f3e3c3b052e35237819e2ee2c1dcfe70a4f
James Wright <james@jamesswright.co.uk>
2015-05-14 21:06:17 +0100

    Pagination and caching

--- routes/getBlog.js
+++ routes/getBlog.js
@@ -2,8 +2,10 @@
 
 var glob = require('glob');
 var fs = require('fs');
+var jonathan = require('jonathan');
 var Showdown = require('showdown');
 var converter = new Showdown.converter();
+var POSTS_PER_PAGE = 5;
 
 function convert(file) {
 	return new Promise(function (resolve, reject) {
@@ -15,11 +17,45 @@ function convert(file) {
 			resolve(converter.makeHtml(content.toString()));
 		});
 	});
-	
+}
+
+function getCacheKey(slug) {
+	var base = 'posts';
+	var pageNumber = parseInt(slug);
+
+	if (!slug) {
+		return base;
+	}
+
+	if (pageNumber) {
+		return base + '-page-' + pageNumber;
+	}
+
+	return base + '-' + slug;
+}
+
+function render(res, posts, page) {
+	var isPaginated = posts.length === 1;
+
+	res.render('blog.html', {
+		posts: posts,
+		page: isPaginated ? page : null,
+		totalPages: isPaginated ? POSTS_PER_PAGE : null
+	});
 }
 
 module.exports = function (req, res, next) {
-	var posts = [];
+	var slug = req.params.slug;
+	var key = getCacheKey(slug);
+	var pageNumber = parseInt(slug) || 0;
+	var isAllPosts = key === 'posts';
+	var posts = jonathan.get(key);
+	var promises = [];
+
+	if (posts) {
+		render(res, posts);
+		return;
+	}
 
 	glob('blogs/**/' + (req.params.slug || '*') + '.md', function (err, files) {
 		if (!files.length) {
@@ -32,16 +68,17 @@ module.exports = function (req, res, next) {
 			return;
 		}
 
-		files.forEach(function (file, i) {
-			convert(file).then(function (markdown) {
-				posts.push(markdown);
+		if (!isAllPosts) {
+			files = files.slice(pageNumber, pageNumber + POSTS_PER_PAGE);
+		}
 
-				if (i === files.length - 1) {
-					res.render('blog.html', {
-						posts: posts
-					});
-				}
-			}).catch(next);
+		promises = files.map(function (file) {
+			return convert(file);
 		});
+
+		Promise.all(promises).then(function (markdowns) {
+			jonathan.add(key, markdowns, (60).minutes);
+			render(res, markdowns);
+		}).catch(next);
 	});
 };
