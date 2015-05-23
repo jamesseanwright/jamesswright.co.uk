7515707a3bc7dc9189ae36cfc3adf1a307c40767
James Wright <james@jamesswright.co.uk>
2015-05-23 11:31:18 +0100

    Test for CI

--- build-scripts/pre-test.sh
+++ build-scripts/pre-test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 npm install -g bower node-sass nodemon mocha mocha-phantomjs uglify-js
 npm i && bower i
-pwd
-ls
+ls public
 echo "Gonna uglify"
 uglifyjs --compress --output public/javascripts/main.min.js -- clientjs/*
