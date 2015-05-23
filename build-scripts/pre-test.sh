bbc199c4851dc2e95855dfc5d3803f2c5423a289
James Wright <james@jamesswright.co.uk>
2015-05-23 11:39:27 +0100

    Now creating public/javascripts dir for CI if needed

--- build-scripts/pre-test.sh
+++ build-scripts/pre-test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 npm install -g bower node-sass nodemon mocha mocha-phantomjs uglify-js
 npm i && bower i
-ls public
-echo "Gonna uglify"
+
+if [ ! -d public/javascripts ]
+	then
+		mkdir public/javascripts
+fi
+
 uglifyjs --compress --output public/javascripts/main.min.js -- clientjs/*
