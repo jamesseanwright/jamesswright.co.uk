9de207b0df82f2a494ccf38f42375bf0fda0892e
James Wright <james@jamesswright.co.uk>
2015-05-23 11:21:54 +0100

    Seeing if sass still compiles on ci

--- tasks/dev.sh
+++ tasks/dev.sh
@@ -2,9 +2,7 @@
 # Starts the server and relaunches when files change
 
 # TODO: watch for changes in clientjs!
-ls
-echo "about to uglify"
-uglifyjs --compress --output public/javascripts/main.min.js -- clientjs/*
+## uglifyjs --compress --output public/javascripts/main.min.js -- clientjs/*
 
 node-sass sass/main.scss public/stylesheets/main.min.css --watch sass/* --output-style compressed &
 SASS_PID=$!
