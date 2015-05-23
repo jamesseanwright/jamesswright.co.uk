a5b3a5b514eccb1435cdd182b963f15b214a17cd
James Wright <james@jamesswright.co.uk>
2015-05-23 16:36:33 +0100

    Added uglify call upon nodemon restart

--- tasks/dev.sh
+++ tasks/dev.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 # Starts the server and relaunches when files change
 
-# TODO: watch for changes in clientjs!
-uglifyjs --compress --output public/javascripts/main.min.js -- clientjs/*
+# uglifyjs is invoked on restart, but rather than have another watcher,
+# nodemon runs it on restart i.e. when clientjs files have changed.
+# The duplication sucks but it's better than Grunt and a load of watch tasks
+uglifyjs --compress --screw-ie8 --output public/javascripts/main.min.js -- clientjs/*
 
 node-sass sass/main.scss public/stylesheets/main.min.css --watch sass/* --output-style compressed &
 SASS_PID=$!
