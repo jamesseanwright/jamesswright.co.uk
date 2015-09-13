ca0080dfbc92484f03677511bff4b4aa4cb07afa
James Wright <james@jamesswright.co.uk>
2015-09-13 23:35:02 +0100

    Resolved release task

--- tasks/release.sh
+++ tasks/release.sh
@@ -3,27 +3,24 @@ echo "Building dist files..."
 
 if [ -e dist/ ]
 then
-	rm -rf dist/
+	rm -rf dist/*
 fi
 
-cp server.js package.json bower.json dist/
-
-cp -R node_modules/ dist/
-cp -R blogs/ dist/
-cp -R data/ dist/
-cp -R init/ dist/
-cp -R models/ dist/
-cp -R public/ dist/
-rm -R dist/public/vendor/
-cp -R routes/ dist/
-cp -R utils/ dist/
-cp -R views/ dist/
-
 cd dist
-git init
+cp ../server.js ../package.json ../bower.json .
+
+cp -R ../node_modules/ .
+cp -R ../blogs/ .
+cp -R ../data/ .
+cp -R ../init/ .
+cp -R ../models/ .
+cp -R ../public/ .
+rm -R public/vendor/
+cp -R ../routes/ .
+cp -R ../utils/ .
+cp -R ../views/ .
+
 git add -A
 git commit -m "Release"
-git remote add heroku https://git.heroku.com/james-wright-prod.git
-git pull heroku master
-git push heroku master
+git push -f heroku master
 cd ..
