401fd430463d4f07b5379609c9579486ea6118d8
James Wright <james@jamesswright.co.uk>
2016-01-21 23:49:34 +0000

    remove gitignore in dist repo. use node 4

--- tasks/release.sh
+++ tasks/release.sh
@@ -13,18 +13,25 @@ git clone https://git.heroku.com/james-wright-prod.git $DIST_DIR
 cd $DIST_DIR
 cp ../server.js ../package.json ../bower.json .
 
-./tasks/build-js.sh
-./tasks/build-sass.sh
+# I'm think initialising a sub repo in Git is
+# copying over the .gitignore file from the parent!
+if [ -e .gitignore ]
+then
+	rm .gitignore
+fi
 
-cp -R ../node_modules
-cp -R ../blogs
-cp -R ../data
-cp -R ../init
-cp -R ../models
-cp -R ../public
-cp -R ../routes
-cp -R ../utils
-cp -R ../views
+
+cp -R ../node_modules .
+cp -R ../blogs .
+cp -R ../data .
+cp -R ../init .
+cp -R ../models .
+cp -R ../public .
+cp -R ../routes .
+cp -R ../utils .
+cp -R ../views .
+
+rm -rf public/vendor
 
 git add -A
 git commit -m "Release"
