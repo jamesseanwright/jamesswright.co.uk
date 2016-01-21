f95948453c31d156c0f766b9ea242468d2c3916c
James Wright <james.wright@net-a-porter.com>
2016-01-21 19:10:44 +0000

    Release

--- tasks/release.sh
+++ tasks/release.sh
@@ -8,23 +8,23 @@ then
 	rm -rf $DIST_DIR
 fi
 
-git clone https://git.heroku.com/james-wright-prod.git $DIST_DIR
-
+#git clone https://git.heroku.com/james-wright-prod.git $DIST_DIR
+mkdir $DIST_DIR
 cd $DIST_DIR
 cp ../server.js ../package.json ../bower.json .
 
-./tasks/build-js.sh
-./tasks/build-sass.sh
+../tasks/build-js.sh
+../tasks/build-sass.sh
 
-cp -R ../node_modules
-cp -R ../blogs
-cp -R ../data
-cp -R ../init
-cp -R ../models
-cp -R ../public
-cp -R ../routes
-cp -R ../utils
-cp -R ../views
+cp -R ../node_modules node_modules
+cp -R ../blogs blogs
+cp -R ../data data
+cp -R ../init init
+cp -R ../models models
+cp -R ../public public
+cp -R ../routes routes
+cp -R ../utils utils
+cp -R ../views views
 
 git add -A
 git commit -m "Release"
