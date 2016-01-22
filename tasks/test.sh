be9f8d4318ae1bf6af231d122bcebefb0da0c686
James Wright <james.wright@net-a-porter.com>
2016-01-22 17:56:56 +0000

    Valimate integration

--- tasks/test.sh
+++ tasks/test.sh
@@ -1,16 +1,24 @@
 #! /bin/bash
 # Runs server and client-side unit tests
 
+function checkForFailure {
+    if [ $? -ne 0 ]
+    then
+        exit $?
+    fi
+}
+
+
 mocha tests/unit/server/*
 
-if [ $? -ne 0 ]
-	then
-		exit $?
-fi
+checkForFailure
 
 NODE_ENV=test node tests/testserver.js &
 NODE_PID=$!
 trap "kill NODE_PID" SIGTERM SIGKILL
 mocha-phantomjs http://localhost:3001
 kill $NODE_PID
-exit $?
+
+checkForFailure
+
+valimate
