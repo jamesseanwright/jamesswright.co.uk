2eb02bf266c1cccb405f1d19a15ce78913fa9cc3
James Wright <james@jamesswright.co.uk>
2019-11-17 21:52:33 +0000

    Updates Reno version

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@v0.23.0/http/server.ts";
-import { createRouter, createRouteMap, textResponse } from "https://raw.githubusercontent.com/jamesseanwright/reno/v0.5.0/reno/mod.ts";
+import { createRouter, createRouteMap, textResponse } from "https://raw.githubusercontent.com/jamesseanwright/reno/v0.5.1/reno/mod.ts";
 
 const PORT = '8002';
 
@@ -18,6 +18,6 @@ const router = createRouter(
   console.log('Listening for requests...');
 
   for await (const req of serve(`:${PORT}`)) {
-    await req.respond(await router(req));
+    await req.respond(await router(req)).catch(console.error);
   }
-})().catch(console.error);
+})();
