f1b04c54d9752cae1ccd7dbf3a943284cd649820
James Wright <james@jamesswright.co.uk>
2019-11-26 15:57:05 +0000

    Handles app-level errors

---
+++ deps.ts
@@ -0,0 +1,1 @@
+export * from "https://raw.githubusercontent.com/jamesseanwright/reno/v0.6.4/reno/mod.ts";
--- server.ts
+++ server.ts
@@ -1,23 +1,54 @@
-import { serve } from "https://deno.land/std@v0.23.0/http/server.ts";
-import { createRouter, createRouteMap, textResponse } from "https://raw.githubusercontent.com/jamesseanwright/reno/v0.5.1/reno/mod.ts";
+import { serve, ServerRequest } from "https://deno.land/std@v0.23.0/http/server.ts";
+import { createRouter, createRouteMap, textResponse, NotFoundError } from "./deps.ts";
 
 const PORT = '8002';
 
+const formatDate = (date: Date) =>
+  date.toLocaleDateString("en-GB", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "short"
+  });
+
+const logRequest = (req: ServerRequest) => {
+  console.log(`[${formatDate(new Date())}] Request for ${req.url}`);
+};
+
+const createErrorResponse = (status: number, { message }: Error) => ({
+  status,
+  ...textResponse(message)
+});
+
+// TODO: use HTTP's Status enum
+const notFound = (e: NotFoundError) => createErrorResponse(404, e);
+const serverError = (e: Error) => createErrorResponse(500, e);
+
+const onError = (e: Error) => {
+  console.error(e);
+  return e instanceof NotFoundError ? notFound(e) : serverError(e);
+};
+
 const router = createRouter(
   createRouteMap([
-    ["/home", () => textResponse(
-      '<p>Hello world!</p>',
-      new Headers({
-        'Content-Type': 'text/html',
-      })
+    ["/", () => textResponse(
+      "<p>Hello world!</p>",
+      {
+        "Content-Type": "text/html",
+      }
     )]
   ]),
 );
 
 (async () => {
-  console.log('Listening for requests...');
+  console.log("Listening for requests...");
 
   for await (const req of serve(`:${PORT}`)) {
-    await req.respond(await router(req)).catch(console.error);
+    logRequest(req);
+
+    await req.respond(await router(req).catch(onError));
   }
 })();
